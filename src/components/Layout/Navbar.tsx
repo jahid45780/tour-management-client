5fa7185305ea9359781d5f82c32c2b051abf009e
jahid45780 <jahidhossain88899@gmail.com>
2026-06-12 01:09:52 +0600

    add the role

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -1,122 +1,3 @@
-// import { useState } from "react";
-// import { Menu, X } from "lucide-react";
-// import { ModeToggle } from "../modeTheam/mode-toggle";
-// import { Link } from "react-router-dom";
-// import { useLogoutMutation, useUserInfoQuery,  } from "@/redux/features/auth/auth.api";
-
-// const Navbar = () => {
-//   const [open, setOpen] = useState(false);
-//   const{ data}= useUserInfoQuery(undefined)
-//   const [logout] = useLogoutMutation()
-//  console.log(data?.data?.email);
-
-
-//   const navItems = [
-//     { name: "Home", link: "/" },
-//     { name: "About", link: "/about" },
-//     { name: "Tours", link: "/tours" },
-//     { name: "Contact", link: "/contact" },
-//   ];
-
-//   const handleLogout = async()=>{
-//       logout(undefined)
-//   }
-
-//   return (
-//     <header className="w-full sticky top-0 z-50 border-b border-gray-200 dark:border-gray-800 bg-background/80 backdrop-blur-md">
-//       <div className="max-w-7xl mx-auto px-5 lg:px-8">
-//         <div className="flex items-center justify-between h-20">
-//           {/* Logo */}
-//           <a href="/" className="flex items-center gap-3">
-//             <div className="w-11 h-11 rounded-full bg-cyan-500 flex items-center justify-center text-white font-bold text-xl shadow-lg shadow-cyan-500/30">
-//               T
-//             </div>
-
-//             <h1 className="text-2xl font-extrabold tracking-wide text-foreground">
-//               Tour<span className="text-cyan-500">Wave</span>
-//             </h1>
-//           </a>
-
-//           {/* Desktop Menu */}
-//           <nav className="hidden md:flex items-center gap-8">
-//             {navItems.map((item) => (
-//              <Link
-//             key={item.name}
-//             to={item.link}
-//             className="relative text-muted-foreground font-medium transition-all duration-300 hover:text-cyan-500 after:absolute after:left-0 after:-bottom-1 after:h-[2px] after:w-0 after:bg-cyan-500 after:transition-all after:duration-300 hover:after:w-full"
-//     >
-//            {item.name}
-//            </Link>
-//             ))}
-//           </nav>
-
-//           {/* Right Side */}
-//           <div className="hidden md:flex items-center gap-4">
-//             <ModeToggle />
-
-//            <Link to="/login" >
-//              <button className="w-full p-1.5 rounded-full bg-cyan-500 hover:bg-cyan-600 text-white font-medium transition duration-300 shadow-md">
-//               Login
-//             </button>
-//             </Link>
-//           </div>
-
-//           {/* Mobile Controls */}
-//           <div className="md:hidden flex items-center gap-3">
-//             <ModeToggle />
-
-//             <button
-//               onClick={() => setOpen(!open)}
-//               className="text-foreground"
-//             >
-//               {open ? <X size={30} /> : <Menu size={30} />}
-//             </button>
-//           </div>
-//         </div>
-
-//         {/* Mobile Menu */}
-//         <div
-//           className={`md:hidden overflow-hidden transition-all duration-300 ${
-//             open ? "max-h-[500px] pb-6" : "max-h-0"
-//           }`}
-//         >
-//           <div className="mt-4 rounded-2xl border border-border bg-background/95 backdrop-blur-lg shadow-xl p-5 flex flex-col gap-5">
-//             {navItems.map((item) => (
-//                <Link
-//             key={item.name}
-//             to={item.link}
-//             className="relative text-muted-foreground font-medium transition-all duration-300 hover:text-cyan-500 after:absolute after:left-0 after:-bottom-1 after:h-[2px] after:w-0 after:bg-cyan-500 after:transition-all after:duration-300 hover:after:w-full"
-//     >
-//            {item.name}
-//            </Link>
-//             ))}
-
-//             {data?.data?.email && ( 
-//               <button
-//               onClick={handleLogout}
-//               className ="w-full py-3 rounded-full bg-cyan-500 hover:bg-cyan-600 text-white font-medium transition duration-300 shadow-md"
-//               >
-//                 Logout
-//               </button>
-//             )}
-
-//             {!data?.data?.email && (
-//                <Link to="/login" >
-//              <button className="w-full py-3 rounded-full bg-cyan-500 hover:bg-cyan-600 text-white font-medium transition duration-300 shadow-md">
-//               Login
-//             </button>
-//            </Link>
-//             )}
-            
-//           </div>
-//         </div>
-//       </div>
-//     </header>
-//   );
-// };
-
-// export default Navbar;
-
 
 import { useState } from "react";
 import { Menu, X } from "lucide-react";
@@ -128,6 +9,8 @@ import {
   useUserInfoQuery,
 } from "@/redux/features/auth/auth.api";
 import { useAppDispatch } from "@/redux/hooks";
+import { role } from "@/constants/role";
+
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
@@ -137,10 +20,12 @@ const Navbar = () => {
   const dispatch = useAppDispatch();
  console.log(data?.data?.email);
   const navItems = [
-    { name: "Home", link: "/" },
-    { name: "About", link: "/about" },
-    { name: "Tours", link: "/tours" },
-    { name: "Contact", link: "/contact" },
+    { name: "Home", link: "/", role:"PUBLIC" },
+    { name: "About", link: "/about", role:"PUBLIC" },
+    { name: "Tours", link: "/tours", role:"PUBLIC" },
+    { name: "Contact", link: "/contact", role:"PUBLIC" },
+    { name: "Dashboard", link: "/admin", role:role.ADMIN},
+    { name: "Dashboard", link: "/user", role:role.USER},
   ];
 
   // ✅ login check
@@ -163,7 +48,7 @@ const Navbar = () => {
         <div className="flex items-center justify-between h-20">
           
           {/* Logo */}
-          <a href="/" className="flex items-center gap-3">
+          <Link to={"/"} className="flex items-center gap-3" >
             <div className="w-11 h-11 rounded-full bg-cyan-500 flex items-center justify-center text-white font-bold text-xl">
               T
             </div>
@@ -171,16 +56,31 @@ const Navbar = () => {
             <h1 className="text-2xl font-extrabold">
               Tour<span className="text-cyan-500">Wave</span>
             </h1>
-          </a>
+          </Link>
 
-          {/* Desktop Menu */}
-          <nav className="hidden md:flex items-center gap-8">
-            {navItems.map((item) => (
-              <Link key={item.name} to={item.link}>
-                {item.name}
-              </Link>
-            ))}
-          </nav>
+         {/* Desktop Menu */}
+<nav className="hidden md:flex items-center gap-8">
+  {navItems.map((item) => {
+    const canShow =
+      item.role === "PUBLIC" ||
+      (item.role === role.ADMIN &&
+        data?.data?.role === role.ADMIN) ||
+      (item.role === role.USER &&
+        data?.data?.role === role.USER);
+
+    if (!canShow) return null;
+
+    return (
+      <Link
+        key={`${item.name}-${item.link}`}
+        to={item.link}
+        className="relative text-muted-foreground font-medium transition-all duration-300 hover:text-cyan-500 after:absolute after:left-0 after:-bottom-1 after:h-[2px] after:w-0 after:bg-cyan-500 after:transition-all after:duration-300 hover:after:w-full"
+      >
+        {item.name}
+      </Link>
+    );
+  })}
+</nav>
 
           {/* Right Side (Desktop) */}
           <div className="hidden md:flex items-center gap-4">
@@ -211,43 +111,53 @@ const Navbar = () => {
             </button>
           </div>
         </div>
+       
+    {/* Mobile Menu */}
+<div
+  className={`md:hidden overflow-hidden transition-all duration-300 ${
+    open ? "max-h-[500px] pb-6" : "max-h-0"
+  }`}
+>
+  <div className="mt-4 flex flex-col gap-5">
+    {navItems.map((item) => {
+      const canShow =
+        item.role === "PUBLIC" ||
+        (item.role === role.ADMIN &&
+          data?.data?.role === role.ADMIN) ||
+        (item.role === role.USER &&
+          data?.data?.role === role.USER);
 
-        {/* Mobile Menu */}
-        <div
-          className={`md:hidden overflow-hidden transition-all duration-300 ${
-            open ? "max-h-[500px] pb-6" : "max-h-0"
-          }`}
+      if (!canShow) return null;
+
+      return (
+        <Link
+          key={`${item.name}-${item.link}`}
+          to={item.link}
+          onClick={() => setOpen(false)}
         >
-          <div className="mt-4 flex flex-col gap-5">
-            
-            {navItems.map((item) => (
-              <Link
-                key={item.name}
-                to={item.link}
-                onClick={() => setOpen(false)}
-              >
-                {item.name}
-              </Link>
-            ))}
+          {item.name}
+        </Link>
+      );
+    })}
 
-            {isLoggedIn ? (
-              <button
-                onClick={handleLogout}
-                className="w-full py-3 rounded-full bg-red-500 text-white"
-              >
-                Logout
-              </button>
-            ) : (
-              <Link to="/login">
-                <button className="w-full py-3 rounded-full bg-cyan-500 text-white">
-                  Login
-                </button>
-              </Link>
-            )}
-
-          </div>
-        </div>
-      </div>
+    {isLoggedIn ? (
+      <button
+        onClick={handleLogout}
+        className="w-full py-3 rounded-full bg-red-500 text-white"
+      >
+        Logout
+      </button>
+    ) : (
+      <Link to="/login">
+        <button className="w-full py-3 rounded-full bg-cyan-500 text-white">
+          Login
+        </button>
+      </Link>
+    )}
+  </div>
+</div>
+    
+       </div>
     </header>
   );
 };
