58bbd9a8eaa498715c1aaf566e35bba7a53c5f4a
jahid45780 <jahidhossain88899@gmail.com>
2026-07-12 14:15:03 +0600

    add aiosins

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -22,15 +22,37 @@ axiosInstance.interceptors.request.use(
 
 // Add a response interceptor
 axiosInstance.interceptors.response.use(
-  function (response) {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
-    return response;
-  },
-  function (error) {
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
-    return Promise.reject(error);
+  // function (response) {
+  //   // Any status code that lie within the range of 2xx cause this function to trigger
+  //   // Do something with response data
+  //   return response;
+  // },
+  // function (error) {
+  //   // Any status codes that falls outside the range of 2xx cause this function to trigger
+  //   // Do something with response error
+  //   return Promise.reject(error);
+  // }
+
+  (response) => {
+       return response;
+  }, async (error) => {
+      if (error.response.status === 500 &&
+       error.response.data.message === 'jwt expired' ) {
+            console.log('your token has expired')
+
+            try {
+               const res  = await axiosInstance.post('/auth/refresh-token')
+               console.log('new  token arrived', res)
+            } catch (error){
+              console.error(error)
+            }
+
+       }
+
+      //  for Everything else, 
+
+       return Promise.reject(error)
   }
+
 );
 
